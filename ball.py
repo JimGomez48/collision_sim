ccaea92647c4ee19e1242b533fe967f9b43254de
justinalanbass <justinalanbass@gmail.com>
2014-05-29 16:51:39 -0700

    Simplified position updates

--- ball.py
+++ ball.py
@@ -6,7 +6,7 @@ import colors
 
 class Ball(Object3D):
 
-    def __init__(self, color, initxp, inityp, initzp, initxdv, initydv, initzdv):
+    def __init__(self, color, initxp, inityp, initzp, initxv, inityv, initzv):
         Object3D.__init__(self)
         self.color = color
         
@@ -14,10 +14,9 @@ class Ball(Object3D):
         self.inityp = inityp
         self.initzp = initzp
         
-        self.initxv = self.inityv = self.initzv = 0
-        self.initxdv = initxdv
-        self.initydv = initydv
-        self.initzdv = initzdv
+        self.initxv = initxv
+        self.inityv = inityv
+        self.initzv = initzv
 
     def update(self, delta):
         glMatrixMode(GL_MODELVIEW)
@@ -35,10 +34,10 @@ class Ball(Object3D):
         glColor4fv(self.color)
         glMaterialfv(GL_FRONT, GL_SPECULAR, colors.WHITE)
         glMateriali(GL_FRONT, GL_SHININESS, 60)
+        self.initxp += self.initxv
+        self.inityp += self.inityv
+        self.initzp += self.initzv
         glTranslatef(self.initxp, self.inityp, self.initzp)
-        self.initxv += self.initxdv
-        self.inityv += self.initydv
-        self.initzv += self.initzdv
-        glTranslatef(self.initxv, self.inityv, self.initzv)
+        
         glutSolidSphere(80, 30, 20)
         glPopMatrix()
