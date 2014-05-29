39246aa1ba6cd39f4b3ad0ea446b2cf606c416a6
justinalanbass <justinalanbass@gmail.com>
2014-05-29 16:46:32 -0700

    Created TopDown scene object
    Initializes 10 balls with a random position and a random inwardly-oriented velocity, to maximize number of collisions.
    
    This scene crashes often after rendering for several seconds when there are many objects.

---
+++ octtree_topdown_scene.py
@@ -0,0 +1,29 @@
+from scene import Scene
+import colors
+from ball import Ball
+from volume import Volume
+
+import random
+random.seed()
+
+class OctTreeTopDownScene(Scene):
+    def __init__(self):
+        super(OctTreeTopDownScene, self).__init__()
+        for i in range(10):
+            initxp = random.randint(-500,500)
+            inityp = random.randint(-500,500)
+            initzp = random.randint(-500,500)
+            initxdv = random.randint(-1,1) - initxp/100
+            initydv = random.randint(-1,1) - inityp/100
+            initzdv = random.randint(-1,1) - initzp/100
+            self.add_object_3d(Ball(colors.BLUE, initxp, inityp, initzp, initxdv, initydv, initzdv))
+        
+        self.add_object_3d(Volume(colors.WHITE))
+
+    def update(self, delta):
+        # call the super class update method
+        super(OctTreeTopDownScene, self).update(delta)
+
+    def draw(self):
+        # call the super class draw method
+        super(OctTreeTopDownScene, self).draw()
