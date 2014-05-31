875fa2879a4f46be8bfde0034e9579e957c7789d
Utkarsh Jaiswal <utkarshjaiswal91@gmail.com>
2014-05-31 01:57:12 -0700

    More updates to sweep_and_prune_scene.py

--- sweep_and_prune_scene.py
+++ sweep_and_prune_scene.py
@@ -6,13 +6,17 @@ from volume import Volume
 from pprint import pprint
 
 import random
+import sys
 random.seed()
 
 NUM_OBJECTS = 15 # Number of objects in the scene
 
 class SAPScene(Scene):
 
-    PosList = xList = yList = zList = []
+    PosList = []
+    xList = []
+    yList = []
+    zList = []
     def __init__(self):
         super(SAPScene, self).__init__()
         for i in range(NUM_OBJECTS):
@@ -59,24 +63,51 @@ class SAPScene(Scene):
             self.zList.append(zListElement)
 
         self.add_object_3d(Volume(colors.WHITE))
-
+        
     def update(self, delta):
         
-        ''' As per the sweep and prune (aka sort and sweep) algorithm, position lists for each axis are built initially and sorted in each frame '''
+        # even though the lists are initialized by the constructor, they must be updated each time since object positions change
+
+        # get a list of the objects' X, Y and Z coordinates + its index and store it in PosList
+        for i in range(NUM_OBJECTS):
+            PosListElement = []
+            PosListElement.append(i)
+            PosListElement.append(self.objects_3d[i].get_position()[0])
+            PosListElement.append(self.objects_3d[i].get_position()[1])
+            PosListElement.append(self.objects_3d[i].get_position()[2])
+            self.PosList.append(PosListElement)
+
+        # currently collecting start and end of AABB as position (x,y,z) +- side/2 instead of axial projections
+        # store start and end of object boundaries along each axis, used to determine if collision is occurring
+
+        for i in range(NUM_OBJECTS):
+            xListElement = []
+            xListElement.append(i)
+            xListElement.append(self.objects_3d[i].get_position()[0] - self.objects_3d[i].side/2)
+            xListElement.append(self.objects_3d[i].get_position()[0] + self.objects_3d[i].side/2)
+            self.xList.append(xListElement)
+
+        for i in range(NUM_OBJECTS):
+            yListElement = []
+            yListElement.append(i)
+            yListElement.append(self.objects_3d[i].get_position()[1] - self.objects_3d[i].side/2)
+            yListElement.append(self.objects_3d[i].get_position()[1] + self.objects_3d[i].side/2)
+            self.yList.append(yListElement)
+        
+        for i in range(NUM_OBJECTS):
+            zListElement = []
+            zListElement.append(i)
+            zListElement.append(self.objects_3d[i].get_position()[2] - self.objects_3d[i].side/2)
+            zListElement.append(self.objects_3d[i].get_position()[2] + self.objects_3d[i].side/2)
+            self.zList.append(zListElement)
 
         # sort all 3 lists based on the beginning positions of the AABBs
         self.xList.sort(key = lambda el: el[1])
         self.yList.sort(key = lambda el: el[1])
         self.zList.sort(key = lambda el: el[1])
 
-        #for x in self.xList:
-        #    print x
-        #for y in self.yList:
-        #    print y
-        #for z in self.zList:
-        #    print z
-
         # check for collisions
+
         # if so, react by changing their velocities
 
         # call the super class update method
