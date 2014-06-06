43b20735d514e3f419c42eaab0113a0387e85a7b
James Gomez <jim.gomez.48@gmail.com>
2014-06-05 22:05:06 -0700

    Added simple KdTree implementation

--- kd_tree_scene.py
+++ kd_tree_scene.py
@@ -1,4 +1,5 @@
-import random
+import random as rand
+import numpy as np
 
 from scene import Scene
 from ball import *
@@ -7,13 +8,54 @@ from vector3 import *
 import colors
 
 
-class KdTreeNode:
-    def __init__(self, position=Point3()):
-        self.position = position
-        self.left = None
-        self.right = None
-        self.children = []
-        # TODO everything else
+class KdNode:
+    def __init__(self, obj, axis, left, right):
+        self.obj = obj
+        self.axis = axis
+        self.left = left
+        self.right = right
+
+    # def __str__(self):
+    #     """
+    #     print using in-order traversal
+    #     """
+    #     self.__in_order_print__(self)
+    #
+    # def __in_order_print__(self, node):
+    #     """
+    #     simple in-order traversal
+    #     """
+    #     if node is None:
+    #         return
+    #
+    #     self.__in_order_print__(node.left)
+    #     print node.obj.position()
+    #     self.__in_order_print__(node.right)
+
+
+def make_kd_tree(objs, dimensions=3, depth=0):
+    """
+    Recursively builds a KdTree
+
+    :param objs: The object to store at this node
+    :param dimensions: the number od dimensions to split along
+    :param depth: the current depth of tree
+    :return: a fully built KdTree
+    """
+    axis = depth % dimensions
+    objs = sorted(objs, key=lambda obj: obj.position()[axis])
+    median = len(objs) // 2
+
+    if len(objs) == 0 or objs is None:
+        return None
+
+    return KdNode(
+        obj=objs[median],
+        axis=axis,
+        left=make_kd_tree(objs[:median], dimensions, depth + 1),
+        right=make_kd_tree(objs[median + 1:], dimensions, depth + 1),
+    )
+
 
 class KdTreeScene(Scene):
     def __init__(self, num_objects=50):
@@ -21,22 +63,23 @@ class KdTreeScene(Scene):
         origin = Point3()
         for i in range(self.num_objects):
             position = Point3(
-                random.randint(-500, 500),
-                random.randint(-500, 500),
-                random.randint(-500, 500)
+                rand.randint(-500, 500),
+                rand.randint(-500, 500),
+                rand.randint(-500, 500)
             )
             ball = CollidableBall(
                 color=colors.BLUE,
                 radius=30,
-                mass=random.randint(20, 100),
+                mass=rand.randint(20, 100),
                 start_p=position,
-                start_v=Vector3(0, 0, random.randint(200, 500))  # forward velocity
+                start_v=Vector3(0, 0, rand.randint(200, 500))  # forward velocity
             )
             ball.update(1)  # force OM to update before turn_to_face
             ball.turn_to_face_p(origin)
             ball.rotate(5, Vector3(0, 1, 0))
             self.add_object_3d(ball)
         self.add_object_3d(Volume(colors.MAGENTA))
+        self.kd_tree = make_kd_tree(self.objects_3d)
 
     def update(self, delta):
         # call the super class update method
