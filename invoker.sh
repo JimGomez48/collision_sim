4891af11fb7a96fa219a941caf9704a30b9d0b6b
Utkarsh Jaiswal <utkarshjaiswal91@gmail.com>
2014-06-11 01:38:34 -0700

    Updated shell script

---
+++ invoker.sh
@@ -0,0 +1,23 @@
+echo "Simulation Name,Objects,Sim Time, Last Frame, Min FPS, Max FPS, Min Compared, Max Compared" > results.csv
+
+for n in 128 256 512 1024
+do
+    #No collisions
+    python main.py 1 $n --sim_time 10
+    
+    #Brute Force
+    python main.py 2 $n --sim_time 10
+    
+    #Octree
+    for l in `seq 1 9`
+    do
+        python main.py 3 $n --octree_levels $l --sim_time 10
+    done
+
+    # K-D Tree    
+    python main.py 4 $n -s 10    
+
+    # Sweep and Prune
+    python main.py 5 $n -s 10
+    
+done
